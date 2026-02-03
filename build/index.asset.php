b0025cbb652218986d1b8da99f9f2801c61f2cbb
Carsten Bach <mail@carsten-bach.de>
2026-02-03 09:48:08 +0100

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-core-data', 'wp-data', 'wp-date', 'wp-editor', 'wp-element', 'wp-plugins'), 'version' => '9aadf8a9a3349d954862');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-core-data', 'wp-data', 'wp-date', 'wp-editor', 'wp-element', 'wp-plugins'), 'version' => '07de547a4fafe86ff20f');
